100d29c6ed0dccccdd54e421c814675f4285228c
adama-sio <60093105+adama-sio@users.noreply.github.com>
2020-01-29 14:39:00 +0100

    pUSH DERN

--- Bracelets/Listes_Form5.Designer.cs
+++ Bracelets/Listes_Form5.Designer.cs
@@ -28,19 +28,321 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Listes_Form5));
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.lbTypeRegroupement = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.chbx = new System.Windows.Forms.CheckBox();
+            this.txbxNomCourt = new System.Windows.Forms.TextBox();
+            this.txbxNomPlan = new System.Windows.Forms.TextBox();
+            this.txbxCodeType = new System.Windows.Forms.TextBox();
+            this.lbCheckbox = new System.Windows.Forms.Label();
+            this.lbTypeDePlan = new System.Windows.Forms.Label();
+            this.lbTypeGestion = new System.Windows.Forms.Label();
+            this.lbLesPlanDeChasse = new System.Windows.Forms.Label();
+            this.lbNomCourt = new System.Windows.Forms.Label();
+            this.lbNomTypePlanChasse = new System.Windows.Forms.Label();
+            this.lbCodeType = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbxTypeRegroupement = new System.Windows.Forms.ComboBox();
+            this.cmbxTypeGestion = new System.Windows.Forms.ComboBox();
+            this.cmbx = new System.Windows.Forms.ComboBox();
+            this.cmbxConcerne = new System.Windows.Forms.ComboBox();
+            this.label10 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox3.Location = new System.Drawing.Point(630, 319);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(20, 5);
+            this.pictureBox3.TabIndex = 171;
+            this.pictureBox3.TabStop = false;
+            // 
+            // lbTypeRegroupement
+            // 
+            this.lbTypeRegroupement.AutoSize = true;
+            this.lbTypeRegroupement.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTypeRegroupement.Location = new System.Drawing.Point(92, 242);
+            this.lbTypeRegroupement.Name = "lbTypeRegroupement";
+            this.lbTypeRegroupement.Size = new System.Drawing.Size(408, 24);
+            this.lbTypeRegroupement.TabIndex = 170;
+            this.lbTypeRegroupement.Text = "Type de regroupement de gestion de NumPlan";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox2.Location = new System.Drawing.Point(630, 235);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(20, 5);
+            this.pictureBox2.TabIndex = 169;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Location = new System.Drawing.Point(645, 235);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(5, 89);
+            this.pictureBox1.TabIndex = 168;
+            this.pictureBox1.TabStop = false;
+            // 
+            // chbx
+            // 
+            this.chbx.AutoSize = true;
+            this.chbx.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chbx.Location = new System.Drawing.Point(527, 457);
+            this.chbx.Name = "chbx";
+            this.chbx.Size = new System.Drawing.Size(15, 14);
+            this.chbx.TabIndex = 167;
+            this.chbx.UseVisualStyleBackColor = true;
+            // 
+            // txbxNomCourt
+            // 
+            this.txbxNomCourt.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbxNomCourt.Location = new System.Drawing.Point(527, 196);
+            this.txbxNomCourt.Name = "txbxNomCourt";
+            this.txbxNomCourt.Size = new System.Drawing.Size(100, 29);
+            this.txbxNomCourt.TabIndex = 166;
+            // 
+            // txbxNomPlan
+            // 
+            this.txbxNomPlan.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbxNomPlan.Location = new System.Drawing.Point(528, 145);
+            this.txbxNomPlan.Name = "txbxNomPlan";
+            this.txbxNomPlan.Size = new System.Drawing.Size(462, 29);
+            this.txbxNomPlan.TabIndex = 165;
+            // 
+            // txbxCodeType
+            // 
+            this.txbxCodeType.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbxCodeType.Location = new System.Drawing.Point(527, 95);
+            this.txbxCodeType.Name = "txbxCodeType";
+            this.txbxCodeType.Size = new System.Drawing.Size(100, 29);
+            this.txbxCodeType.TabIndex = 164;
+            // 
+            // lbCheckbox
+            // 
+            this.lbCheckbox.AutoSize = true;
+            this.lbCheckbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCheckbox.Location = new System.Drawing.Point(134, 451);
+            this.lbCheckbox.Name = "lbCheckbox";
+            this.lbCheckbox.Size = new System.Drawing.Size(366, 24);
+            this.lbCheckbox.TabIndex = 163;
+            this.lbCheckbox.Text = "Ce type de plan gère-t-il les minimas à tuer";
+            // 
+            // lbTypeDePlan
+            // 
+            this.lbTypeDePlan.AutoSize = true;
+            this.lbTypeDePlan.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTypeDePlan.Location = new System.Drawing.Point(143, 399);
+            this.lbTypeDePlan.Name = "lbTypeDePlan";
+            this.lbTypeDePlan.Size = new System.Drawing.Size(357, 24);
+            this.lbTypeDePlan.TabIndex = 162;
+            this.lbTypeDePlan.Text = "Ce type de plan concerne principalement";
+            // 
+            // lbTypeGestion
+            // 
+            this.lbTypeGestion.AutoSize = true;
+            this.lbTypeGestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTypeGestion.Location = new System.Drawing.Point(80, 291);
+            this.lbTypeGestion.Name = "lbTypeGestion";
+            this.lbTypeGestion.Size = new System.Drawing.Size(420, 24);
+            this.lbTypeGestion.TabIndex = 161;
+            this.lbTypeGestion.Text = "Type des gestion des communes dans NumPlan";
+            // 
+            // lbLesPlanDeChasse
+            // 
+            this.lbLesPlanDeChasse.AutoSize = true;
+            this.lbLesPlanDeChasse.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbLesPlanDeChasse.Location = new System.Drawing.Point(86, 335);
+            this.lbLesPlanDeChasse.Name = "lbLesPlanDeChasse";
+            this.lbLesPlanDeChasse.Size = new System.Drawing.Size(414, 48);
+            this.lbLesPlanDeChasse.TabIndex = 160;
+            this.lbLesPlanDeChasse.Text = "Les plans de chasse sont regroupés pour les \r\ndocuments sous-commission et commis" +
+    "sion par";
+            // 
+            // lbNomCourt
+            // 
+            this.lbNomCourt.AutoSize = true;
+            this.lbNomCourt.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNomCourt.Location = new System.Drawing.Point(267, 196);
+            this.lbNomCourt.Name = "lbNomCourt";
+            this.lbNomCourt.Size = new System.Drawing.Size(233, 24);
+            this.lbNomCourt.TabIndex = 159;
+            this.lbNomCourt.Text = "Nom court du type de plan";
+            // 
+            // lbNomTypePlanChasse
+            // 
+            this.lbNomTypePlanChasse.AutoSize = true;
+            this.lbNomTypePlanChasse.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNomTypePlanChasse.Location = new System.Drawing.Point(222, 145);
+            this.lbNomTypePlanChasse.Name = "lbNomTypePlanChasse";
+            this.lbNomTypePlanChasse.Size = new System.Drawing.Size(278, 24);
+            this.lbNomTypePlanChasse.TabIndex = 158;
+            this.lbNomTypePlanChasse.Text = "Nom du type de plan de chasse";
+            // 
+            // lbCodeType
+            // 
+            this.lbCodeType.AutoSize = true;
+            this.lbCodeType.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCodeType.Location = new System.Drawing.Point(404, 100);
+            this.lbCodeType.Name = "lbCodeType";
+            this.lbCodeType.Size = new System.Drawing.Size(96, 24);
+            this.lbCodeType.TabIndex = 157;
+            this.lbCodeType.Text = "Code type";
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = global::Bracelet.Properties.Resources.logout__1_;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(1085, 40);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(38, 35);
+            this.button2.TabIndex = 156;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(885, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 35);
+            this.button1.TabIndex = 155;
+            this.button1.Text = "Modification";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(109, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(722, 39);
+            this.label1.TabIndex = 154;
+            this.label1.Text = "Paramétrage des types de plans de chasse";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // cbxTypeRegroupement
+            // 
+            this.cbxTypeRegroupement.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxTypeRegroupement.FormattingEnabled = true;
+            this.cbxTypeRegroupement.Location = new System.Drawing.Point(527, 242);
+            this.cbxTypeRegroupement.Name = "cbxTypeRegroupement";
+            this.cbxTypeRegroupement.Size = new System.Drawing.Size(100, 28);
+            this.cbxTypeRegroupement.TabIndex = 175;
+            // 
+            // cmbxTypeGestion
+            // 
+            this.cmbxTypeGestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbxTypeGestion.FormattingEnabled = true;
+            this.cmbxTypeGestion.Location = new System.Drawing.Point(527, 287);
+            this.cmbxTypeGestion.Name = "cmbxTypeGestion";
+            this.cmbxTypeGestion.Size = new System.Drawing.Size(100, 28);
+            this.cmbxTypeGestion.TabIndex = 176;
+            // 
+            // cmbx
+            // 
+            this.cmbx.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbx.FormattingEnabled = true;
+            this.cmbx.Location = new System.Drawing.Point(527, 335);
+            this.cmbx.Name = "cmbx";
+            this.cmbx.Size = new System.Drawing.Size(100, 28);
+            this.cmbx.TabIndex = 177;
+            // 
+            // cmbxConcerne
+            // 
+            this.cmbxConcerne.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbxConcerne.FormattingEnabled = true;
+            this.cmbxConcerne.Location = new System.Drawing.Point(527, 395);
+            this.cmbxConcerne.Name = "cmbxConcerne";
+            this.cmbxConcerne.Size = new System.Drawing.Size(100, 28);
+            this.cmbxConcerne.TabIndex = 178;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(656, 245);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(398, 72);
+            this.label10.TabIndex = 172;
+            this.label10.Text = "Ces deux informations peuvent être modifiées \r\nseulement avant la première créati" +
+    "on de plan \r\nde chasse pour ce type de plan de chasse !";
             // 
             // Listes_Form5
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1264, 814);
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1264, 749);
+            this.Controls.Add(this.cmbxConcerne);
+            this.Controls.Add(this.cmbx);
+            this.Controls.Add(this.cmbxTypeGestion);
+            this.Controls.Add(this.cbxTypeRegroupement);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.lbTypeRegroupement);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.chbx);
+            this.Controls.Add(this.txbxNomCourt);
+            this.Controls.Add(this.txbxNomPlan);
+            this.Controls.Add(this.txbxCodeType);
+            this.Controls.Add(this.lbCheckbox);
+            this.Controls.Add(this.lbTypeDePlan);
+            this.Controls.Add(this.lbTypeGestion);
+            this.Controls.Add(this.lbLesPlanDeChasse);
+            this.Controls.Add(this.lbNomCourt);
+            this.Controls.Add(this.lbNomTypePlanChasse);
+            this.Controls.Add(this.lbCodeType);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Listes_Form5";
             this.Text = "Listes_Form5";
+            this.Load += new System.EventHandler(this.Listes_Form5_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label lbTypeRegroupement;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox chbx;
+        private System.Windows.Forms.TextBox txbxNomCourt;
+        private System.Windows.Forms.TextBox txbxNomPlan;
+        private System.Windows.Forms.TextBox txbxCodeType;
+        private System.Windows.Forms.Label lbCheckbox;
+        private System.Windows.Forms.Label lbTypeDePlan;
+        private System.Windows.Forms.Label lbTypeGestion;
+        private System.Windows.Forms.Label lbLesPlanDeChasse;
+        private System.Windows.Forms.Label lbNomCourt;
+        private System.Windows.Forms.Label lbNomTypePlanChasse;
+        private System.Windows.Forms.Label lbCodeType;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbxTypeRegroupement;
+        private System.Windows.Forms.ComboBox cmbxTypeGestion;
+        private System.Windows.Forms.ComboBox cmbx;
+        private System.Windows.Forms.ComboBox cmbxConcerne;
+        private System.Windows.Forms.Label label10;
     }
 }
