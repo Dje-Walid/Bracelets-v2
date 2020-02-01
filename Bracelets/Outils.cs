c129c9c9c268a3d6bb75a78c69a32d9c75f8d324
adama-sio <60093105+adama-sio@users.noreply.github.com>
2020-02-01 23:26:18 +0100

    AJOUT CLASSE OUTIL + METHODE (LIEN VERS UNE PAGE , ANNULER et Effacer )

---
+++ Bracelets/Outils.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Bracelet
+{
+    class Outils
+    {
+        public Outils()
+        { }
+        public void annuler(Form a)
+        {
+            DialogResult resulta;
+            resulta = MessageBox.Show("Êtes-vous sûr de vouloir annuler ", "AVERTISSEMENT", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+            if (resulta == System.Windows.Forms.DialogResult.OK)
+            {
+                a.Close();
+            }
+            else
+            {
+
+            }
+        }
+        public void effacer(Form a)
+        {
+            foreach (Control c in a.Controls)
+            {
+                if (c is TextBox)
+                {
+                    c.Text = String.Empty;
+                }
+            }
+        }
+
+        public void lien(Form a, Form nomDeLaForm)
+        {
+            a = new Form();
+            nomDeLaForm.Show();
+        }
+    }
+}
